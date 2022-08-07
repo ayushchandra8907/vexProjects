d0f5f0a32d48b0867e0cd486af149d458050b917
waknuckles1020 <71286983+waknuckles1020@users.noreply.github.com>
2022-08-06 21:44:47 -0500

    creation of pros file

---
+++ prosTesting1/include/globals.h
@@ -0,0 +1,9 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include "main.h"
+
+extern pros::Controller controller1;
+extern pros::Motor motor1;
+
+#endif
---
+++ prosTesting1/src/globals.cpp
@@ -0,0 +1,4 @@
+#include "globals.h"
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+pros::Motor motor1(2, pros::E_MOTOR_GEARSET_18, false);
