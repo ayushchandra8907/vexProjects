ae69169076be83b027eff6fefc5f1bf15f272c73
waknuckles1020 <71286983+waknuckles1020@users.noreply.github.com>
2022-09-03 16:17:58 -0500

    globals

---
+++ tankPROS/include/globals.h
@@ -0,0 +1,13 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include "main.h"
+
+extern pros::Motor rfm;
+
+
+
+
+
+
+#endif
---
+++ tankPROS/src/globals.cpp
@@ -0,0 +1,4 @@
+#include "main.h"
+#include "globals.h"
+
+pros::Motor rfm(1, pros::E_MOTOR_GEARSET_18, false);
