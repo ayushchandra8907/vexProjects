b704fc1789a1b5fbf56bf418122c92b49d733afc
waknuckles1020 <71286983+waknuckles1020@users.noreply.github.com>
2022-09-06 20:12:29 -0500

    sadf

--- tankPROS/src/globals.cpp
+++ tankPROS/src/globals.cpp
@@ -2,3 +2,6 @@
 #include "globals.h"
 
 pros::Motor rfm(1, pros::E_MOTOR_GEARSET_18, false);
+pros::Motor lfm(2, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor rbm(3, pros::E_MOTOR_GEARSET_18, false);
+pros::Motor lbm(4, pros::E_MOTOR_GEARSET_18, true);
