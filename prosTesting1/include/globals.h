08200154d3e3f894c7a649a5f0b5df7d8da17a8c
waknuckles1020 <71286983+waknuckles1020@users.noreply.github.com>
2022-09-13 10:09:13 -0500

    cleaning up

--- prosTesting1/include/globals.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBALS_H
-#define GLOBALS_H
-
-#include "main.h"
-
-extern pros::Controller controller1;
-extern pros::Motor RFM;
-
-#endif
--- prosTesting1/src/globals.cpp
+++
@@ -1,4 +0,0 @@
-#include "globals.h"
-
-pros::Controller controller1(pros::E_CONTROLLER_MASTER);
-pros::Motor RFM(2, pros::E_MOTOR_GEARSET_18, false);
